e832f0dcd46f6f396eb81a682d2d79dcd481b132
Damian <harasymczuk@contecht.eu>
2021-02-03 11:50:46 +0100

    find by coordinates function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var regionsGeoBounds_1 = require("./lib/regionsGeoBounds");
+var findByCoordinates_1 = require("./lib/findByCoordinates");
 exports.regionBoundMap = regionsGeoBounds_1.regionBoundMap;
+exports.findByCoordinates = findByCoordinates_1.findByCoordinates;
---
+++ test.js
@@ -0,0 +1,5 @@
+var findByCoordinates = require("./index.js").findByCoordinates;
+
+console.log(findByCoordinates("Denmark", 12.568333, 55.676111));
+
+console.log(findByCoordinates("Finland", 23.766667, 61.500000));
